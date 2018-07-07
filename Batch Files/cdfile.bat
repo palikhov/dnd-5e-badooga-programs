a9aafa0c71d3a3037f90c98fef19b5576489cb58
badooga <33851191+badooga@users.noreply.github.com>
2018-07-07 01:07:04 -0400

    Add files via upload

---
+++ Batch Files/cdfile.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Made by badooga. https://github.com/badooga/Python-Files
+REM Lets you cd to a given directory given that you know the name of a file within it; only use this if the passed file's directory is in the PATH.
+REM To us this, put this in a folder that has its PATH set up in environmental variables and then use as a command in the cmd.
+
+cd %~dp$PATH:1
---
+++ Batch Files/compile.bat
@@ -0,0 +1,23 @@
+@echo off
+REM Made by badooga. https://github.com/badooga/Python-Files
+REM Use to automate the compiling of py files into exe files with no useless files or directories left over.
+REM As seen below, the use of this program requires pyinstaller to be installed on your computer.
+REM You can use this in the cmd by passing the py to be compiled as an argument (with the appropriate filepath, unless the PATH will take care of that.)
+REM You can also use a program like Default Programs Editor (see below) to add this batch file to the .py extension's context menu.
+REM Default Programs Editor: http://defaultprogramseditor.com/
+
+if ["%~dp$PATH:1"] == [] (
+    cd "%~dp1"
+) else (
+    cd "%~dp$PATH:1"
+)
+pyinstaller --distpath "Compiled Applications" --workpath .\exe_build_temp -F %1
+if NOT ["%errorlevel%"]==["0"] (
+    echo "Error: A file called '%1' was not found."
+    pause
+    exit /b %errorlevel%
+)
+set str=%1
+set str=%str:.py=.spec%
+del %str%
+rmdir /S /Q exe_build_temp
