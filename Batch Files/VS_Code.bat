057fd99083784e964c8d7b3f66672dd70456c747
badooga <33851191+badooga@users.noreply.github.com>
2018-07-09 15:49:45 -0400

    Add files via upload

--- Batch Files/VS_Code.bat
+++ Batch Files/VS_Code.bat
@@ -1,10 +1,8 @@
 @echo off
 REM Made by badooga. https://github.com/badooga/Python-Files
 REM Used to open both a supplied file and its parent folder in Visual Studio Code. Requires VS Code to be in the PATH.
-REM If used in the command line, the cmd will not close once this launched (if you can fix this, please make a pull request).
-REM Considering that you need VS Code in the PATH anyway, just don't use this file in the CMD.
-REM Instead, make this program the default editor for the appropriate file extensions using the Control Panel.
-REM You can also edit the context menu's edit button via a program like Default Programs Editor (see below).
+REM You can make this program the default editor for certain file extensions using the Control Panel.
+REM You can also edit the context menu's edit button for a given file extension via a program like Default Programs Editor (see below).
 REM Default Programs Editor: http://defaultprogramseditor.com/
 
 if [%1] == [] (
@@ -19,5 +17,4 @@ if ["%~dp$PATH:1"] == [""] (
     cd %~dp$PATH:1
 )
 start /MAX "" code . "%~nx1"
-CLS
 exit
